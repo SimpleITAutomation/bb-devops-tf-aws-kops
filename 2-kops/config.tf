ebae6bb471126af5d6f3f792130c22ec64d26a33
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-01-24 19:36:26 -0300

    BBL-80 rolling back kops code for tf-0.11 till tf-0.12 is fully supported by kops (https://github.com/kubernetes/kops/issues/7052)

--- 2-kops/config.tf
+++ 2-kops/config.tf
@@ -8,13 +8,13 @@ variable "region" {
 
 # AWS Provider
 provider "aws" {
-    region = var.region
-    profile = var.profile
+    region = "${var.region}"
+    profile = "${var.profile}"
 }
 
 # Backend Config (partial)
 terraform {
-    required_version = ">= 0.12.19"
+    required_version = "= 0.11.14"
 
     backend "s3" {
         key = "dev/k8s/kops/terraform.tfstate"
